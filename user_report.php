37782d8e33b08418679d6e13a88631a54509b7e1
Aaron (wutno) <gxtxnvidia@live.com>
2012-07-05 06:29:56 -0400

    license update

--- user_report.php
+++ user_report.php
@@ -1,4 +1,27 @@
 <?php
+/*  Fuuka Report System
+ *  ------------------------------------------
+ *  Author: wutno (#/g/tv - Rizon)
+ *
+ *
+ *  GNU License Agreement
+ *  ---------------------
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ *  http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
 require_once('libs/recaptchalib.php');
 require_once("config/sql_conf.php");
 
